d9649f3bf0a996b917175e62f1cc08fceed130ee
Kevin Aguilar <kevin.231@hotmail.com>
2021-04-19 20:24:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const colors = require('./shared/colors.shared');
 const { connect } = require('./database/database.connection');
 const components = require('./components/component.routes');
@@ -18,6 +19,10 @@ app.use(cors());
 app.use(express.static(`./test`))
 app.use(express.static(`./public`))
 app.use('/', components);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
   const log = `Server Up on port ${process.env.PORT} ${solidColors.magenta}`
